97d372f9905918a098f97b947aa9dec256f47a88
Oleg <addicted2sounds@gmail.com>
2016-05-26 15:25:33 +0300

    get /users.json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    render json: User.all, each_serializer: UserListSerializer
+  end
+
   def create
     @user = User.new user_params
     if @user.save
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 describe 'Users', type: :request do
-  let(:valid_params) { { name: Faker::Name.name } }
-  let(:invalid_params) { { name: '' } }
-  describe 'registration' do
+  describe 'GET /users.json' do
+    let(:request) { get '/users.json' }
+    let(:subject) { request; json }
+
+    context 'when no users exist' do
+      it { is_expected.to be_empty }
+    end
+
+    context 'when users exist' do
+      let!(:user) { create :user }
+      it 'return users list' do
+        request
+        expect(json.count).to eq 1
+      end
+    end
+  end
+
+  describe 'POST /users.json' do
+    let(:valid_params) { { name: Faker::Name.name } }
+    let(:invalid_params) { { name: '' } }
     context 'when valid params' do
       subject { post '/users.json', user: valid_params }
 
