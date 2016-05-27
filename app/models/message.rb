bc2e17646403f81f2cb1fcc80abdbea0955f76b7
Oleg <addicted2sounds@gmail.com>
2016-05-27 12:38:42 +0300

    updated readme

--- app/models/message.rb
+++ app/models/message.rb
@@ -22,13 +22,11 @@ class Message < ActiveRecord::Base
 
   def update_unread_messages!
     chat_users = chat.chat_users.where.not(user_id: user.id)
-    # chat_users.update_al
-    # p chat_users
     chat_users.update_all('unread_messages_count = unread_messages_count + 1')
-    # chat_users.increment_counter(:unread_messages_count, 1)
   end
 
   def update_user_messages_count!
     user.increment(:messages_count, 1)
+    user.save!
   end
 end
